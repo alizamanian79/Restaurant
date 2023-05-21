894bc8207052721b3a421dfcf7e344010eaeec31
alizamanian79 <120646614+alizamanian79@users.noreply.github.com>
2023-05-21 12:57:13 +0330

    currency factor

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import { Route, Routes } from 'react-router-dom';
 
 // Pages
 import Home from "../src/Components/Pages/Home"
-import FormFood from "../src/Components/Pages/FoodForm"
+
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/restaurant' element={<Home />}></Route>
-        <Route path='/restaurant/formfood' element={<FormFood />}></Route>
+        
       </Routes>
 
     </>
